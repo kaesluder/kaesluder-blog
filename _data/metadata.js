80402850fe3ce97e2acdf38e3a97a2d3c5bc1663
Kae Sluder <kaesluder@fastmail.com>
2025-06-29 15:13:02 -0400

    fix package.json to use pathprefix

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
 	title: "Kae Job Sluder",
-	url: "https://kaesluder.github.io/",
+	url: "https://kaesluder.github.io/kaesluder-blog/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
